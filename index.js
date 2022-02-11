4d540ce95c1ae5ad4f76d498ee12fcb66cde2b4d
Rene Hernandez <rene83120@gmail.com>
2022-02-11 13:40:30 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function (driver) {
+    return (
+      driver.toLowerCase().substring(0, name.length) === name.toLowerCase()
+    );
+  });
+}
+function matchName(drivers, town) {
+  return drivers.filter(function (driver) {
+    return driver.name === town;
+  });
+}
